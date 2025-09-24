089ad1bec08ea7fb0ec388ea89cec3a7a9cf83b7
arkiver <arkiver@protonmail.com>
2025-09-24 15:30:27 +0200

    Version 20250924.01. Attempt ot get ?original on all daumcdn.net/cfile/ URLs.

--- tistory.lua
+++ tistory.lua
@@ -589,7 +589,7 @@ wget.callbacks.get_urls = function(file, url, is_css, iri)
         end
       end
     end]]
-    if string.match(url, "^https?://[^/]*daumcdn%.net/cfile/tistory")
+    if string.match(url, "^https?://[^/]*daumcdn%.net/cfile/")
       and not string.match(url, "%?") then
       check(url .. "?original")
     end
