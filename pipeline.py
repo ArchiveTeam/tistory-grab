e61460ba675b24a1588f8a16bc4b800d0dda9be3
arkiver <arkiver@protonmail.com>
2025-09-25 13:32:27 +0200

    Version 20250925.02. (messed up previous commit message)

--- pipeline.py
+++ pipeline.py
@@ -78,7 +78,7 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20250925.01'
+VERSION = '20250925.02'
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:143.0) Gecko/20100101 Firefox/143.0'
 TRACKER_ID = 'tistory'
 TRACKER_HOST = 'legacy-api.arpa.li'
